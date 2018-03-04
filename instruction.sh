2a911df716c9348b2851b56e5ad6eaf661467bea
root <indianprogrammer.in@gmail.com>
2018-03-05 00:26:22 +0530

    test

--- instruction.sh
+++ instruction.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 $url = "https://raw.githubusercontent.com/indianprogrammer/google-backup/master"
-sudo wget -O key.json $url/key.json
-sudo wget -O gcloud-snapshot.sh $url/gcloud-snapshot.sh
-sudo chmod 777 gcloud-snapshot.sh
-gcloud auth activate-service-account  --key-file=key.json
-/bin/sh gcloud-snapshot.sh
+sudo wget -O key.json $url/key.json && sudo wget -O gcloud-snapshot.sh $url/gcloud-snapshot.sh && sudo chmod 777 gcloud-snapshot.sh && gcloud auth activate-service-account  --key-file=key.json && /bin/sh gcloud-snapshot.sh
