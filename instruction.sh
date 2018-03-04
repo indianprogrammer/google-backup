8df1f7de8a2d2d4a91439b1ac04f441c3b46891f
root <indianprogrammer.in@gmail.com>
2018-03-05 00:14:28 +0530

    test

--- instruction.sh
+++ instruction.sh
@@ -4,3 +4,4 @@ sudo wget -O key.json $url/key.json
 sudo wget -O gcloud-snapshot.sh $url/gcloud-snapshot.sh
 sudo chmod 777 gcloud-snapshot.sh
 gcloud auth activate-service-account  --key-file=key.json
+/bin/sh gcloud-snapshot.sh
